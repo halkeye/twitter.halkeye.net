e9294a2e92b6091a0114bfb822f7153b4ea044f0
Gavin Mogan <git@gavinmogan.com>
2024-03-26 11:30:36 -0700

    build too

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,8 @@
 const numeral = require("numeral");
-const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-const { execSync } = require('child_process')
+const {EleventyHtmlBasePlugin} = require("@11ty/eleventy");
+const {execSync} = require('child_process')
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 	eleventyConfig.ignores.add("README.md");
 
 	// eleventyConfig.setServerPassthroughCopyBehavior("copy");
@@ -11,19 +11,21 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("img/");
 	eleventyConfig.addPassthroughCopy("video/");
 
+	eleventyConfig.addPassthroughCopy("CNAME");
+
 	eleventyConfig.addPassthroughCopy({
 		"node_modules/@11ty/is-land/is-land.js": "assets/is-land.js",
 	});
 
 	eleventyConfig.addJavaScriptFunction("avatarUrl", function avatarUrl(url) {
-		if(url.startsWith("https://twitter.com/")) {
+		if (url.startsWith("https://twitter.com/")) {
 			url = "https://x.com/" + url.slice("https://twitter.com/".length);
 		}
 		return `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(url)}/`;
 	});
 
 	eleventyConfig.addJavaScriptFunction("renderNumber", function renderNumber(num) {
-		if(typeof num === "string") {
+		if (typeof num === "string") {
 			num = parseInt(num, 10);
 		}
 		return numeral(num).format("0,0");
@@ -34,6 +36,6 @@ module.exports = function(eleventyConfig) {
 	// pagefind search plugin
 	eleventyConfig.on('eleventy.after', () => {
 		console.log('[pagefind] Creating search index.');
-		execSync(`npx pagefind --source _site --glob \"[0-9]*/**/*.html\"`, { encoding: 'utf-8' });
-  });
+		execSync(`npx pagefind --source _site --glob \"[0-9]*/**/*.html\"`, {encoding: 'utf-8'});
+	});
 };

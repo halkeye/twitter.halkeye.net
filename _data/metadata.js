b2911b251176db1ffce758059b6444cabc7c89bd
Gavin Mogan <git@gavinmogan.com>
2024-03-26 15:28:27 -0700

    fix metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "halkeye", // No leading @ here
+	homeLabel: "halkeye.net",
+	homeUrl: "https://www.halkeye.net/",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
